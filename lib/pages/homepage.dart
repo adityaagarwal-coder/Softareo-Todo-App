52b91fb6db635efa65af1bebedff1268c422b589
A <2020kucp1009@iiitkota.ac.in>
2021-09-30 20:18:31 +0530

    initial commit

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,101 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_7/tile_layout.dart';
+import 'package:flutter_application_7/database.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+String userId = "1:1007450016897:web:e97bccda9dd6507a509180";
+
+class _HomeState extends State<Home> {
+  late Stream taskStream;
+
+  DatabaseServices databaseServices = new DatabaseServices();
+  TextEditingController taskEdittingControler = new TextEditingController();
+  @override
+  void initState() {
+    databaseServices.getTasks(userId).then((val) {
+      taskStream = val;
+      setState(() {});
+    });
+
+    super.initState();
+  }
+
+  Widget taskList() {
+    return StreamBuilder(
+      stream: taskStream,
+      builder: (context, snapshot) {
+        return snapshot.hasData
+            ? ListView.builder(
+                padding: EdgeInsets.only(top: 10),
+                itemCount: (snapshot.data! as QuerySnapshot).docs.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Tile(
+                    (snapshot.data! as QuerySnapshot).docs[index]
+                        ["isCompleted"],
+                    (snapshot.data! as QuerySnapshot).docs[index]["task"],
+                    (snapshot.data! as QuerySnapshot).docs[index].id,
+                  );
+                })
+            : Container();
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey,
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              const Text("TO DO APP"),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: taskEdittingControler,
+                      decoration: const InputDecoration(hintText: "task"),
+                      onChanged: (text) {
+                        taskEdittingControler.text = text;
+                        setState(() {});
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  taskEdittingControler.text.isNotEmpty
+                      ? GestureDetector(
+                          onTap: () {
+                            Map<String, dynamic> taskMap = {
+                              "task": taskEdittingControler.text,
+                              "isCompleted": false
+                            };
+
+                            databaseServices.createTask(
+                                userId, taskMap); // Create Todo
+                            taskEdittingControler.text = "";
+                          },
+                          child: Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 12, vertical: 5),
+                              child: Text("ADD")))
+                      : Container()
+                ],
+              ),
+              taskList()
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/tile_layout.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_7/database.dart';
+import 'package:flutter_application_7/pages/homepage.dart';
+
+// The widget "Tile" here is the layout of each task getting displayed dynamically on the screen
+class Tile extends StatefulWidget {
+  final bool isCompleted;
+  final String task;
+  final String documentId;
+  Tile(this.isCompleted, this.task, this.documentId);
+
+  @override
+  _TileState createState() => _TileState();
+}
+
+class _TileState extends State<Tile> {
+  late bool edittext;
+  TextEditingController taskEdittingControler = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            GestureDetector(
+                onTap: () {
+                  Map<String, dynamic> taskMap = {
+                    "isCompleted": !widget.isCompleted
+                  };
+                  DatabaseServices()
+                      .updateTask(userId, taskMap, widget.documentId);
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Colors.black,
+                          width: 1), // Mark Todo as Updated
+                      borderRadius: BorderRadius.circular(30)),
+                  child: widget.isCompleted
+                      ? Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        )
+                      : Container(),
+                )),
+            SizedBox(
+              width: 6,
+            ),
+            edittext
+                ? Text(
+                    widget.task, // Text Editor for each task
+                    style: TextStyle(fontSize: 15),
+                  )
+                : Expanded(
+                    child: TextField(
+                      controller: taskEdittingControler,
+                      onChanged: (text) {
+                        taskEdittingControler.text = text;
+                        setState(() {});
+                      },
+                    ),
+                  ),
+            SizedBox(
+              width: 8,
+            ),
+            GestureDetector(
+                // Edit Todo
+                child: Icon(
+                  Icons.edit,
+                  size: 10,
+                  color: Colors.black,
+                ),
+                onTap: () {
+                  Map<String, dynamic> taskMap = {
+                    "task": taskEdittingControler.text,
+                    "isCompleted": false
+                  };
+                  edittext = true;
+                  DatabaseServices().updateTask(
+                      userId, taskMap, widget.documentId); //Edit Todo
+                }),
+            SizedBox(
+              width: 8,
+            ),
+            GestureDetector(
+              onTap: () {
+                DatabaseServices().deleteTask(userId, widget.documentId);
+              },
+              child: Icon(Icons.close,
+                  size: 10, color: Colors.black), // Delete Todo
+            )
+          ],
+        ));
+  }
+}
