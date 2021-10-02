c020d2bc6bf2360119f27fc2242daf2ffde29dc3
adityaagarwal-coder <82513880+adityaagarwal-coder@users.noreply.github.com>
2021-10-03 04:20:20 +0530

    Update database.dart

--- lib/database.dart
+++ lib/database.dart
@@ -1,22 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseServices {
-  updateTask(String userId, Map<String, dynamic> taskMap, String documentId) {
+  updateTask(Map<String, dynamic> taskMap, String documentId) {
     FirebaseFirestore.instance
         .collection("tasks")
         .doc(documentId)
         .set(taskMap, SetOptions(merge: true));
   }
 
-  dynamic createTask(String userId, Map<String, dynamic> taskMap) {
+  dynamic createTask(Map<String, dynamic> taskMap) {
     FirebaseFirestore.instance.doc(userId).collection("tasks").add(taskMap);
   }
 
-  getTasks(String userId) async {
+  getTasks() async {
     return await FirebaseFirestore.instance.collection("tasks").snapshots();
   }
 
-  deleteTask(String userId, String documentId) {
+  deleteTask(String documentId) {
     FirebaseFirestore.instance
         .collection("tasks")
         .doc(documentId)
